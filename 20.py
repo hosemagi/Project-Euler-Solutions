622989b055e4fc81537c9387dfddc80d8d4ee30b
Preston <prestonclewis@gmail.com>
2011-05-28 23:51:35 -0700

    added comments to 20

--- 20.py
+++ 20.py
@@ -2,20 +2,34 @@ import math
 from datetime import datetime
 
 print "Solving..."
-starttime = datetime.now()
+starttime = datetime.now()                      # record start time for time profiling
 
+# define a function that takes a number and returns the sum of its digits
 def sumOfDigits(number):
+    # convert the number to a string representation
     stringRepresentation = str(number)
+    
+    # start the running sum at 0
     sumOfDigits = 0
+    
+    # for each character at index 'i' in the string representation of the number
     for i in range(len(stringRepresentation)):
+        # convert the digit into an integer
         digit = int(stringRepresentation[i])
+        
+        #add the integer to the running sum
         sumOfDigits += digit
+        
+    # return the calculated sum of the digits
     return sumOfDigits
 
+# get the value of 100!
 oneHundredFactorial = math.factorial(100)
+
+# pass the integer value of 100! to the sumOfDigits function and print its return value
 print sumOfDigits(oneHundredFactorial)
 
 
-endtime = datetime.now()
-elapsed = endtime - starttime
-print "Solution took " + str(elapsed)
+endtime = datetime.now()                        # record end time for time profiling
+elapsed = endtime - starttime                   # calculate running time
+print "Solution took " + str(elapsed)           # print running time
