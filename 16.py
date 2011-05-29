02be79bb4f9c7d36f5e6563f03b6335d9c20184b
Preston <prestonclewis@gmail.com>
2011-05-28 23:57:58 -0700

    solved 16

---
+++ 16.py
@@ -0,0 +1,35 @@
+import math
+from datetime import datetime
+
+print "Solving..."
+starttime = datetime.now()                      # record start time for time profiling
+
+# define a function that takes a number and returns the sum of its digits
+def sumOfDigits(number):
+    # convert the number to a string representation
+    stringRepresentation = str(number)
+    
+    # start the running sum at 0
+    sumOfDigits = 0
+    
+    # for each character at index 'i' in the string representation of the number
+    for i in range(len(stringRepresentation)):
+        # convert the digit into an integer
+        digit = int(stringRepresentation[i])
+        
+        #add the integer to the running sum
+        sumOfDigits += digit
+        
+    # return the calculated sum of the digits
+    return sumOfDigits
+
+# get the value of 2^1000
+num = 2**1000
+
+# pass the integer value of 100! to the sumOfDigits function and print its return value
+print sumOfDigits(num)
+
+
+endtime = datetime.now()                        # record end time for time profiling
+elapsed = endtime - starttime                   # calculate running time
+print "Solution took " + str(elapsed)           # print running time
